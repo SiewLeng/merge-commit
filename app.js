87140254811d52879850ba9405eeac596bd5e798
SiewLeng <siewleng.lim@gmail.com>
2021-10-14 10:01:20 +0800

    route-b-1

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('/route-a-2', function(req, res) {
   res.status(200).json('This is route-a-2 page');
 });
 
+app.get('/route-b-1', function(req, res) {
+  res.status(200).json('This is route-b-1 page');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port);
 console.log(`Server is listening on port ${port}`);
