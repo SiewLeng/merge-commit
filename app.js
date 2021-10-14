0fd59df4b2384551c88a71f38b95ca69c7e8049d
SiewLeng <siewleng.lim@gmail.com>
2021-10-14 09:57:51 +0800

    add route-a-2

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/route-a-1', function(req, res) {
   res.status(200).json('This is route-a-1 page');
 });
 
+app.get('/route-a-2', function(req, res) {
+  res.status(200).json('This is route-a-2 page');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port);
 console.log(`Server is listening on port ${port}`);
