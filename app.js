998de0e3735b45f82996b5936a4aea86642e4bae
SiewLeng <siewleng.lim@gmail.com>
2021-10-14 09:55:09 +0800

    route-a-1

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res) {
   res.status(200).json('This is Index Page');
 });
 
+app.get('/route-a-1', function(req, res) {
+  res.status(200).json('This is route-a-1 page');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port);
 console.log(`Server is listening on port ${port}`);
