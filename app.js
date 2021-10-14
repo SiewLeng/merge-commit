500aca17dc4cdf198dd55dbfeed3036f0bd2f486
SiewLeng <siewleng.lim@gmail.com>
2021-10-14 10:03:07 +0800

    add route-b-2

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.get('/route-b-1', function(req, res) {
   res.status(200).json('This is route-b-1 page');
 });
 
+app.get('/route-b-2', function(req, res) {
+  res.status(200).json('This is route-b-2 page');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port);
 console.log(`Server is listening on port ${port}`);
